db76edca873cc7bc682b3d872bac7a486f4a2bf2
GrogDeFrog <george.knightly@gmail.com>
2024-07-20 17:15:14 -0700

    Fix tone generator

--- html/tones/js/pentatonicTones.js
+++ html/tones/js/pentatonicTones.js
@@ -1,11 +1,31 @@
+let musicPlaying = false;
+let intervalID;
+
 function getRandomPentatonicNote() {
-  const pentatonicNotes = ['C', 'D', 'E', 'G', 'A'];
-  return pentatonicNotes[Math.floor(Math.random() * pentatonicNotes.length)];
+    const pentatonicNotes = ['C', 'D', 'E', 'G', 'A'];
+    return pentatonicNotes[Math.floor(Math.random() * pentatonicNotes.length)];
 }
 
 function playRandomPentatonicNote() {
-  setInterval(() => {
-    const note = getRandomPentatonicNote();
-    playTone(note);
-  }, 500);
+    intervalID = setInterval(() => {
+        const note = getRandomPentatonicNote();
+        playTone(note);
+    }, 500);
+}
+
+/* Manage the button */
+function toggleMusic() {
+    const button = document.getElementById("musicButton");
+
+    if (!musicPlaying) {
+        playRandomPentatonicNote();
+
+        button.innerHTML = "Stop Music :(";
+        musicPlaying = true;
+    } else {
+        clearInterval(intervalID);
+
+        button.innerHTML = "Start Music!";
+        musicPlaying = false;
+    }
 }
